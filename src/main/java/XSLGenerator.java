09f0def2bb44313c4acbfa95cd469247c1622cda
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2019-05-29 19:58:44 +0200

    replace the old countries.xml file with the new one

--- src/main/java/XSLGenerator.java
+++ src/main/java/XSLGenerator.java
@@ -1,9 +1,22 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class XSLGenerator {
     private String continent, language;
     private int minSize, maxSize;
 
     public XSLGenerator(){
 
+    }
+
+    public void generate(){
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        try{
+           final DocumentBuilder builder = factory.newDocumentBuilder();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void setContinent(String continent) {
